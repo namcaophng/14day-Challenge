e5691193fcbb10e5edb9d5d5b54f90f02d6848b0
namcaophng <namcaophng@gmail.com>
2020-05-14 08:15:33 +0700

    Task#23225: Create UI for Edit Screen

--- app/src/main/java/com/sunasterisk/a14day_challenge/ui/edit/EditUserFragment.kt
+++ app/src/main/java/com/sunasterisk/a14day_challenge/ui/edit/EditUserFragment.kt
@@ -7,17 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sunasterisk.a14day_challenge.R
 
-class EditFragment private constructor() : Fragment() {
+class EditUserFragment private constructor() : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false)
+        return inflater.inflate(R.layout.fragment_edit_user, container, false)
     }
 
     companion object {
-        fun newInstance() = EditFragment()
+        fun newInstance() = EditUserFragment()
     }
 }
